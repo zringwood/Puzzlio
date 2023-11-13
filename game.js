f4fe55fb018369ccb29dda2d62a0fd4491ba5d27
Zachary Ringwood <zringwood@gmail.com>
2023-11-13 15:55:53 -0500

    Boilerplate implemented

---
+++ game.js
@@ -0,0 +1,21 @@
+class Puzzle extends Phaser.Scene
+    {
+        preload ()
+        {
+           
+        }
+
+        create ()
+        {
+           
+        }
+    }
+
+    const config = {
+        type: Phaser.AUTO,
+        width: 800,
+        height: 600,
+        scene: Puzzle,
+    };
+
+    const game = new Phaser.Game(config);
